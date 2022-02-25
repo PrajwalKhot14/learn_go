4226c26111a5683a02e24dfd6361dece2debc76a
Prajwal <khotprajwal04@gmail.com>
2022-02-25 11:54:26 -0600

    Update ifelse.go

--- ifelse.go
+++ ifelse.go
@@ -6,4 +6,15 @@ func main() {
 	} else {
 		println("Odd")
 	}
+
+	var n = 10
+
+	if n < 0 {
+		println("Negative")
+	} else if n < 9 {
+		println("Less than 10")
+	} else {
+		println("Greater than 10")
+	}
+
 }
